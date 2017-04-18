e63a71bfcc4bde4082cba0017ea3f916f14178c2
JoeJoeJoe352 <sez520@gmail.com>
2017-04-18 20:40:47 +0200

    course listing works. 2 new interfaces, session, beautiful html angular code

--- src/app/my-courses/my-courses.component.ts
+++ src/app/my-courses/my-courses.component.ts
@@ -16,24 +16,41 @@ export class MyCoursesComponent implements OnInit {
   courses: Courses[];
   username: string;
   userid:number;
+  errorMessage:string;
+  subscribedCourses: Courses[];
 
   constructor(private router:Router, private getDataService: GetDataService,) { 
     this.getDataService.getMyCourses().subscribe(
     mycourses => this.mycourses = mycourses);
-
     this.getDataService.getCourses().subscribe(
     courses => this.courses = courses);
+    this.showCourses();
+    
   }
 
   ngOnInit() {
+    
+  }
+
+  showCourses(){
     if(sessionStorage.getItem("name") == undefined){
       this.router.navigate(['/login']);
     }else {
       this.username = sessionStorage.getItem("name");
       this.userid = parseInt(sessionStorage.getItem("userid"));
     }
-
+    //for(var u of this.courses){}
 
   }
 
+/*
+
+    this.errorMessage = this.mycourses.length.toString();
+
+for(var mc of this.mycourses){
+      if(mc.userid == this.userid){
+        this.subscribedCourses = mc.courses;
+      }
+    }
+    */
 }
